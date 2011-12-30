f73e6450f4788fcc55c18b7378a89af6a3f19a78
Benedikt Sauter <sauter@embedded-projects.net>
2011-12-30 10:43:01 +0000

    git-svn-id: svn://localhost/tmp/usbprog/trunk@6 9f48c015-1d5f-4e84-b494-196bf391f123

--- simpleport/examples/SimplePortCSharp/SimplePortCSharp/Program.cs
+++ simpleport/examples/SimplePortCSharp/SimplePortCSharp/Program.cs
@@ -3,65 +3,47 @@ using System;
 
 
 
-    [StructLayout(LayoutKind.Sequential, Pack=8)]
+    [StructLayout(LayoutKind.Sequential, Pack=1)]
     public struct Msimpleport
     {
-        //public IntPtr usb_handle;
-         public IntPtr usb_handle;
-        //byte[] usb_handl
+        public Int32 usb_handle;
     }
 
     struct simpleportDLL {
-   [DllImport("simpleport.dll")]
+    [DllImport("simpleport.dll",CallingConvention=CallingConvention.Cdecl)]
     static public extern void simpleport_open(ref Msimpleport m);
-   [DllImport("simpleport.dll")]
+    [DllImport("simpleport.dll", CallingConvention = CallingConvention.Cdecl)]
     static public extern void simpleport_close(ref Msimpleport m);
-   [DllImport("simpleport.dll")]
-    static public extern void simpleport_set_pin_dir(ref Msimpleport m, Int32 pin, Int32 value);        
+    [DllImport("simpleport.dll", CallingConvention = CallingConvention.Cdecl)]
+    static public extern void simpleport_set_pin_dir(ref Msimpleport m, Int32 pin, Int32 value);
+    [DllImport("simpleport.dll", CallingConvention = CallingConvention.Cdecl)]
+    static public extern void simpleport_set_pin(ref Msimpleport m, Int32 pin, Int32 value);        
     };
 
-
-    /*[DllImport("simpleport.dll")]
-    private static extern void simpleport_open(
-        ref Msimpleport s
-        );
-   
-    [DllImport("simpleport.dll")]
-    private static extern void simpleport_close(
-     ref simpleport s
-    );
-
-    [DllImport("simpleport.dll")]
-    public static extern int simpleport_set_pin_dir(
-     ref simpleport s,
-     int pin,
-     int dir
-    );
-
-    [DllImport("simpleport.dll")]
-    private static extern int simpleport_set_pin(
-     ref simpleport s,
-     int pin,
-     int value
-    );
-    */
 
     class Test
     {
         static void Main()
-    {
-
-            
+        {       
             Msimpleport mysimpleport = new Msimpleport();
             simpleportDLL.simpleport_open(ref mysimpleport);
+
+            simpleportDLL.simpleport_set_pin_dir(ref mysimpleport, 11, 1);
+            
             System.Console.WriteLine("open ready");
             
-            simpleportDLL.simpleport_set_pin_dir(ref mysimpleport, 11, 0);
-            System.Threading.Thread.Sleep(500);
-            simpleportDLL.simpleport_set_pin_dir(ref mysimpleport, 11, 1);
-            System.Threading.Thread.Sleep(500);
-            simpleportDLL.simpleport_set_pin_dir(ref mysimpleport, 11, 0);
+            int count = 4;
+            for (int i = 1; i <= count; i++)
+            {
+                System.Threading.Thread.Sleep(1000);   
+                simpleportDLL.simpleport_set_pin(ref mysimpleport, 11, 0);
+                System.Threading.Thread.Sleep(1000);
+                simpleportDLL.simpleport_set_pin(ref mysimpleport, 11, 1);
+            }
+            
+            simpleportDLL.simpleport_set_pin(ref mysimpleport, 11, 0);
+
             simpleportDLL.simpleport_close(ref mysimpleport);
 
-    }
+        }
     }
